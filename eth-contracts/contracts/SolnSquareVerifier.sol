1c801b2b3f4e8c42e0ad112d4542dcf8b4e110d5
desmond <desmond@gramophone.co.in>
2019-09-29 22:22:56 +0530

    test cases

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -21,22 +21,22 @@ contract SolnSquareVerifier is ERC721Mintable {
     event solutionAdded(uint256 tokenId,address to);
 
     // TODO define a solutions struct that can hold an index & an address
-    struct solutions {
-        uint256 tokenId;
+    struct Solutions{
+       uint tokenId;
         address to;
     }
 
     // TODO define an array of the above struct
-    solutions[] solutionsArr;
+    Solutions[] solutionsArr;
 
     // TODO define a mapping to store unique solutions submitted
-    mapping (bytes32=>solutions) uinqueSolutions;
+    mapping (bytes32=>Solutions) uniqueSolutions;
 
     // TODO Create a function to add the solutions to the array and emit the event
-    function addSolution(uint256 tokenId,address to,bytes32 key) public {
-        solutions memory soln = solutions({tokenId:tokenId,to:to});
+    function addSolution(uint tokenId,address to,bytes32 key) public {
+        Solutions memory soln = Solutions({tokenId:tokenId,to:to});
         solutionsArr.push(soln);
-        uinqueSolutions[key] = soln;
+        uniqueSolutions[key] = soln;
         emit solutionAdded(tokenId,to);
     }
     
@@ -50,13 +50,23 @@ contract SolnSquareVerifier is ERC721Mintable {
         uint[2] memory c,
         uint[2] memory input,
         address to,
-        uint256 tokenId
+        uint tokenId
     ) public {
         bytes32 key = keccak256(abi.encodePacked(a,b,c,input));
-        require(uinqueSolutions[key].to == address(0),'This solution has already been used beofre');
+        require(uniqueSolutions[key].to == address(0),'This solution has already been used before');
         require(verifier.verifyTx(a,b,c,input),'Invalid solution');
         addSolution(tokenId,to,key);
         super.mint(to,tokenId);
+    }
+
+    function getSolutions(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory input)
+        public view returns(uint){
+            bytes32 key = keccak256(abi.encodePacked(a,b,c,input));
+            return uniqueSolutions[key].tokenId;
     }
 }
 
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,35 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+const SquareVerifier = artifacts.require('SquareVerifier');
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+const validProof = require('../../zokrates/code/square/proof');
 
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+contract('TestSolnSquareVerifier', accounts => {
+
+    const account_one = accounts[0];
+    
+    describe('Test Soln Square Verifier', function () {
+        beforeEach(async function () { 
+            const squareVerifier = await SquareVerifier.new({from:account_one});
+            this.contract = await SolnSquareVerifier.new(squareVerifier.address,{from: account_one});        
+        })
+
+        // Test if a new solution can be added for contract - SolnSquareVerifier
+        it('Should be able to add a new solution', async function () {
+            await this.contract.mintNftToken(validProof.proof.a,validProof.proof.b,validProof.proof.c,validProof.inputs,account_one,1);
+            var token = await this.contract.getSolutions(validProof.proof.a,validProof.proof.b,validProof.proof.c,validProof.inputs);
+            assert.equal(token.toNumber(),1,'Solution is not added');
+        });
+
+        // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+        it('Should be able to mint a new token',async function(){
+            var result = true;
+            try {
+                await this.contract.mintNftToken(validProof.proof.a,validProof.proof.b,validProof.proof.c,validProof.inputs,account_one,12);
+            } catch (error) {
+                result = false;
+            }
+            assert.equal(result,true);
+        });
+    });
+    
+})
